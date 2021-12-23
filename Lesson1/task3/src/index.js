08561ace6b9b3a268afe292eed9e185e3cd3dd5e
Aleksandr <stepanetsaleksandr@gmail.com>
2021-12-23 10:16:22 +0200

    324234

--- Lesson1/task3/src/index.js
+++ Lesson1/task3/src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
+// // import React from "react";
+// // import ReactDOM from "react-dom";
+// import "./styles.css";
 
 const rootElement = document.querySelector("#root");
 
