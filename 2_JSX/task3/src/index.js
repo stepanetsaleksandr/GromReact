b0b637983e4c97560181b82839bf33dbc0eea02c
Aleksandr <stepanetsaleksandr@gmail.com>
2021-12-28 16:06:41 +0200

    2342

--- 2_JSX/task3/src/index.js
+++ 2_JSX/task3/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
-const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector("#root");
 
-const renderSeconds = time => {
+const renderSeconds = (time) => {
   const seconds = time.getSeconds();
-  const backgrondColor = seconds % 2 === 0 ? '#fff' : '#000';
-  const textColor = seconds % 2 === 0 ? '#000' : '#fff';
+  const backgrondColor = seconds % 2 === 0 ? "#fff" : "#000";
+  const textColor = seconds % 2 === 0 ? "#000" : "#fff";
 
   const styles = {
     color: textColor,
@@ -22,4 +22,4 @@ const renderSeconds = time => {
 
   ReactDOM.render(element, rootElement);
 };
-setInterval(() => renderSeconds(new Date()));
+setInterval(() => renderSeconds(new Date()), 1000);
