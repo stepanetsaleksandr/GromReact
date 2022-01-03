c3a78fc33faf4b026682d3764b9cc2eb74fc9344
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-03 19:32:24 +0200

    213123

--- 3_Components/task5/src/Profile.jsx
+++ 3_Components/task5/src/Profile.jsx
@@ -4,10 +4,10 @@ import moment from "moment";
 const Profile = (props) => {
   const element = (
     <>
-      <div className="profile__name">{`${props.user.firstName} ${props.user.lastName}`}</div>
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
       <div className="profile__birth">
-        {`Was born ${moment(props.user.birthDate).format("DD MMM YY")} in ${
-          props.user.birthPlace
+        {`Was born ${moment(props.userData.birthDate).format("DD MMM YY")} in ${
+          props.userData.birthPlace
         }`}
       </div>
     </>
--- 3_Components/task5/src/index.jsx
+++ 3_Components/task5/src/index.jsx
@@ -12,4 +12,4 @@ const userData = {
   birthPlace: "London",
 };
 
-ReactDOM.render(<Profile user={userData} />, rootElement);
+ReactDOM.render(<Profile userData={userData} />, rootElement);
