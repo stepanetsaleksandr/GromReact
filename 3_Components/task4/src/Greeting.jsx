22b2f4be2122dfbe9d26e5c962aae3a0d628cbe5
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-03 18:41:11 +0200

    234234

--- 3_Components/task4/src/Greeting.jsx
+++ 3_Components/task4/src/Greeting.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
+import moment from "moment";
 
-const DEFAULT_YEAR = 1970;
-/* 
-1. Получаем дату рождения и переводим в милисекунды
-2. получаем настоящее время в миллисек. и отнимаем от него пункт 1)
-3. берем getYears от получившегося
-*/
-const getAge = birthDate => {
-  const age = new Date().getTime() - birthDate.getTime();
-  return new Date(age).getFullYear() - DEFAULT_YEAR;
+const getAge = (birthDate) => {
+  const age = moment([2007, 0, 29]).fromNow();
+  return age;
 };
 
-const Greeting = props => {
+const Greeting = (props) => {
   const element = (
     <div className="greeting">
-      {`My name is ${props.firstName} ${props.lastName}. I'm ${getAge(props.birthDate)} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${getAge(
+        props.birthDate
+      )} years old`}
     </div>
   );
 
--- 3_Components/task4/src/index.jsx
+++ 3_Components/task4/src/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Greeting from './Greeting.jsx';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import Greeting from "./Greeting.jsx";
+import "./index.scss";
 
-const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector("#root");
 
 ReactDOM.render(
-  <Greeting firstName={'John'} lastName={'Doe'} birthDate={new Date(2004, 9, 8)} />,
-  rootElement,
+  <Greeting
+    firstName={"John"}
+    lastName={"Doe"}
+    birthDate={new Date(2020, 1, 1)}
+  />,
+  rootElement
 );
