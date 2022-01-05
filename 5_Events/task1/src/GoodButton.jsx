871addb0aab61690cb5577d4b1984c9228e886ca
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-05 20:00:25 +0200

    234234

---
+++ 5_Events/task1/src/GoodButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GoodButton = () => (
+  <button
+    className="fancy-button"
+    onClick={() => {
+      alert("Good job!");
+    }}
+  >
+    Click me!
+  </button>
+);
+
+export default GoodButton;
---
+++ 5_Events/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import GoodButton from "./GoodButton.jsx";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<GoodButton />, rootElement);
