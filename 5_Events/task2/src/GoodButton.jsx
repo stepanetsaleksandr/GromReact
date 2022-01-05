fc7021826419b0f9dac961d1e02a2265d1bc90fb
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-05 20:10:55 +0200

    234234

---
+++ 5_Events/task2/src/GoodButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class GoodButton extends React.Component {
+  handleClick() {
+    alert("Good job!");
+  }
+
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+
+export default GoodButton;
