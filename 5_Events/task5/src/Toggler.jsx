ce8f291642630f697a93ec22bddcfd50ffedf8f6
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-05 22:15:22 +0200

    234234

--- 5_Events/task5/src/Toggler.jsx
+++ 5_Events/task5/src/Toggler.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
 class Toggler extends React.Component {
-  handleClick(event) {
-    event.target.textContent === "Off"
-      ? (event.target.textContent = "On")
-      : (event.target.textContent = "Off");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      condition: "Off",
+    };
   }
+
+  setCondition = (event) => {
+    this.setState({
+      condition: event.target.textContent === "Off" ? "On" : "Off",
+    });
+  };
 
   render() {
     return (
-      <button className="toggler" onClick={this.handleClick}>
-        Off
-      </button>
+      <div onClick={this.setCondition} className="toggler">
+        {this.state.condition}
+      </div>
     );
   }
 }
