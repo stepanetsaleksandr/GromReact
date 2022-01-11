663d98722a535b0ec3da33a94b1f0fad43c5dc40
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-11 10:33:29 +0200

    23423432

---
+++ 5_Events/task6/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import ColorPicker from "./ColorPicker.jsx";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<ColorPicker />, rootElement);
