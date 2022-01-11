6a9e8707c76caa486de6f8a49ea48672c85665ac
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-11 11:10:05 +0200

    234234

---
+++ 6_Conditional_rendering/task1/src/Logout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logout = (props) => {
+ 
+  return (
+    <button className="logout" onClick={props.onLogout}>
+      Logout
+    </button>
+  );
+};
+export default Logout;
