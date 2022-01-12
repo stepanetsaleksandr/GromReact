66083e46543cd7c8f94b51a6f4df872adef0f2cd
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-12 22:19:17 +0200

    qweqweqweqwe

--- 8_Lifecycle_methods/task4/src/User.jsx
+++ 8_Lifecycle_methods/task4/src/User.jsx
@@ -5,29 +5,26 @@ class User extends Component {
     user: null,
   };
   componentDidMount() {
-    this.fetchUser(this.props.userId)
-   
+    this.fetchUser(this.props.userId);
   }
-  fetchUser=(userId)=>{ fetch(`https://api.github.com/users/${userId}`)
-  .then((response) => response.json())
-  .then((data) =>
-    this.setState({
-      user: data,
-    })
-  );}
+  fetchUser = (userId) => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          user: data,
+        })
+      );
+  };
   render() {
-    const {user}=this.state;
+    const { user } = this.state;
     if (!user) {
       return null;
     }
-    const{avatar_url,location,name}=user;
+    const { avatar_url, location, name } = user;
     return (
       <div className="user">
-        <img
-          alt="User Avatar"
-          src={avatar_url}
-          className="user__avatar"
-        />
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
         <div className="user__info">
           <span className="user__name">{name}</span>
 
--- 8_Lifecycle_methods/task4/src/index.jsx
+++ 8_Lifecycle_methods/task4/src/index.jsx
@@ -5,5 +5,4 @@ import User from "./User.jsx";
 
 const rootElement = document.querySelector("#root");
 
-
 ReactDOM.render(<User userId="facebook" />, rootElement);
