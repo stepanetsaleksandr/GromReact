022e170653bdafda9bc295a872980ca003a5d543
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-17 13:12:55 +0200

    234234

---
+++ 9_Forms/task1/src/Search.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    value: "good restaurant",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  search = (event) => {
+    event.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.value}
+          className="search__input"
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
