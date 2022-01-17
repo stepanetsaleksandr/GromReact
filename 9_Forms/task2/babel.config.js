7d3dc9f8a0c448725ebfb09e6511fd1f01151892
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-17 13:37:38 +0200

    123123

--- 9_Forms/task2/babel.config.js
+++ 9_Forms/task2/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: '> 1%', useBuiltIns: 'usage', corejs: 3 }],
-    '@babel/preset-react',
+    ["@babel/preset-env", { targets: "> 1%", useBuiltIns: "usage", corejs: 3 }],
+    "@babel/preset-react",
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: ["@babel/plugin-proposal-class-properties"],
 };
