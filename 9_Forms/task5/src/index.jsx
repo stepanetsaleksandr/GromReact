41e1ff182a3b1f4f2a62d87e228a2b8d328c7fe9
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-18 14:33:21 +0200

    234234

---
+++ 9_Forms/task5/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Game from "./Game.jsx";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Game />, rootElement);
