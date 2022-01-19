35e85df2b97a47464d9ff27d92be157d424d03e4
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-19 10:04:33 +0200

    234

---
+++ 10_Lifting_State_up/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./Calculator.jsx";
+import "./index.scss";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Calculator />, rootElement);
