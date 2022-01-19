addf93c34c874df0c48707584e0a53850ec60125
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-19 13:11:58 +0200

    234234

---
+++ 11_Component_Update/task1/src/Numbers.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class Numbers extends PureComponent {
+  render() {
+    return (
+      <div className="number">
+        <span className="number__title">{this.props.title}</span>
+        <span className="number__value">{this.props.number}</span>
+      </div>
+    );
+  }
+}
+export default Numbers;
