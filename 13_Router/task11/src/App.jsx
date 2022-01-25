cef9d8b8fc343adc370b3e2c0664b665c2bf6912
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-25 14:36:53 +0200

    213123

---
+++ 13_Router/task11/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Contacts from "./Contacts.jsx";
+import Home from "./Home.jsx";
+import PageNotFound from "./PageNotFound.jsx";
+import Products from "./Products.jsx";
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route path="/products"><Products /></Route>
+          <Route path="/contacts"><Contacts /></Route>
+          <Route path="/*"><PageNotFound /></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
