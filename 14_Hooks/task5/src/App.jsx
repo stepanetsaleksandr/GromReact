41478e511cf8800c5e2627bac71309128fafbe55
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-27 20:09:52 +0200

    23423423423

---
+++ 14_Hooks/task5/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Clock from './Clock.jsx';
+
+const App = () => {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <>
+      <button className="toggle-time-btn" onClick={() => setToggle(!toggle)}>
+        {toggle ? 'hide' : 'show'}
+      </button>
+      <div className="world-time">
+        {toggle && (
+          <>
+            <Clock location="London" offset={0} />
+            <Clock location="Kyiv" offset={2} />
+            <Clock location="New York" offset={-5} />
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ 14_Hooks/task5/src/Clock.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+const Clock = ({ location, offset }) => {
+  const [time, setTime] = useState(getTimeWithOffset(offset).toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setTime(getTimeWithOffset(offset).toLocaleTimeString()),
+      1000,
+    );
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [time]);
+
+  const currentTime = getTimeWithOffset(offset).toLocaleTimeString();
+  return (
+    <div className="clock">
+      <div className="clock__location">{location}</div>
+      <div className="clock__time">{currentTime}</div>
+    </div>
+  );
+};
+
+
+export default Clock;
