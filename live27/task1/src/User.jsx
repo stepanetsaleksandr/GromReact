5027a409f144e82fc7cfdf195dcaf401d9d65e0f
Aleksandr <stepanetsaleksandr@gmail.com>
2022-02-23 11:30:39 +0200

    ыва

--- live27/task1/src/User.jsx
+++ live27/task1/src/User.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-const User = ({ match }) => {
+const User = () => {
   const [userData, setUserData] = React.useState({
     avatarUrl: null,
     name: null,
     location: null,
   });
 
-  const { userId } = useParams();
+  const { userId: userIdParams } = useParams();
 
   const { avatarUrl, name, location } = userData;
 
-  const fetchUserInfo = (userId) => {
-    return fetch(`https://api.github.com/users/${userId}`)
-      .then((responce) => responce.json())
-      .then((userInfo) => {
-        setUserData({
-          avatarUrl: userInfo.avatar_url,
-          name: userInfo.name,
-          location: userInfo.location,
-        });
-      });
+  const fetchUserInfo = async (userId) => {
+    const responce = await fetch(`https://api.github.com/users/${userId}`);
+    const userInfo = await responce.json();
+
+    setUserData({
+      avatarUrl: userInfo.avatar_url,
+      name: userInfo.name,
+      location: userInfo.location,
+    });
   };
 
+  // запускаем фетч, когда меняется юсер айди
   React.useEffect(() => {
-    console.log(userId);
-    fetchUserInfo(match.params.userId);
+    fetchUserInfo(userIdParams);
   }, []);
 
   return (
